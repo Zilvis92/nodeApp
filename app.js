56231be65953dd817ec5f83e3c4e377698be0636
zilvinasblinovas <blinovaszilvinas@gmail.com>
2025-10-13 19:59:05 +0300

    pakuriaiu partials

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ app.listen(3001, () => {
 });
 
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Pamokos'});
+    const blogs = [
+        {title: 'Pirmoji pamoka', santrauka: 'Tai yra pirmoji pamoka apie Node.js'},
+        {title: 'Antroji pamoka', santrauka: 'Šioje pamokoje sužinosite apie Express.js'},
+        {title: 'Trečioji pamoka', santrauka: 'Ši pamoka skirta EJS šablonų varikliui'},
+        {title: 'Ketvirtoji pamoka', santrauka: 'Šioje pamokoje aptarsime duomenų bazes'}
+    ];
+    res.render('index', {title: 'Pamokos', blogs});
 });
 
 app.get('/apie', (req, res) => {
@@ -18,6 +24,8 @@ app.get('/apie', (req, res) => {
 app.get('/blogs/create-blog', (req, res) => {
     res.render('create-blog', {title: 'Kurti pamoka'});
 });
+
+app.use(express.static(__dirname));
 
 app.use((req, res) => {
     res.status(404).render('404', {title: 'Puslapis nerastas'});
